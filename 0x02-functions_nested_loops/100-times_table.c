1ccfb1a51d163991a59546599f52c4f74aab7519
ma-Amine <ma.laklouch@gmail.com>
2023-05-23 11:48:26 -0700

    Fixing tasks  12/14/15/16

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,44 +10,44 @@
 
 void print_times_table(int n)
 {
-    int row, column, result;
+	int row, column, result;
 
-    if (n >= 0 && n < 15)
-    {
-        for (row = 0; row <= n; row++)
-        {
-            for (column = 0; column <= n; column++)
-            {
-                result = row * column;
+	if (n >= 0 && n < 15)
+	{
+		for (row = 0; row <= n; row++)
+		{
+			for (column = 0; column <= n; column++)
+			{
+				result = row * column;
 
-                if (column == 0)
-                    _putchar('0');
-                else if (result < 10)
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                    _putchar(' ');
-                    _putchar(' ');
-                    _putchar(result % 10 + '0');
-                }
-                else if (result >= 10 && result < 100)
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                    _putchar(' ');
-                    _putchar((result / 10) % 10 + '0');
-                    _putchar(result % 10 + '0');
-                }
-                else if (result > 99 && result < 1000)
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                    _putchar(result / 100 + '0');
-                    _putchar((result / 10) % 10 + '0');
-                    _putchar(result % 10 + '0');
-                }
-            }
-            _putchar('\n');
-        }
-    }
+				if (column == 0)
+					_putchar('0');
+				else if (result < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(result % 10 + '0');
+				}
+				else if (result >= 10 && result < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((result / 10) % 10 + '0');
+					_putchar(result % 10 + '0');
+				}
+				else if (result > 99 && result < 1000)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(result / 100 + '0');
+					_putchar((result / 10) % 10 + '0');
+					_putchar(result % 10 + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
