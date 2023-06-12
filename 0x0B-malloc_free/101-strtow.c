7c8f9455b16ad5c9ae948294e6acd6ff8a13f44a
ma-Amine <ma.laklouch@gmail.com>
2023-06-12 09:18:37 -0700

    Fixing the error in task 16 again

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,16 +1,14 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * free_gr - frees a 2 dimensional array.
- *
- * @grid: array of char.
+ * ch_free_grid - frees a 2 dimensional array.
+ * @grid: multidimensional array of char.
  * @height: height of the array.
  *
- * Return: the number of words
+ * Return: no return
  */
-void free_gr(char **grid, unsigned int height)
+void ch_free_grid(char **grid, unsigned int height)
 {
 	if (grid != NULL && height != 0)
 	{
@@ -22,11 +20,10 @@ void free_gr(char **grid, unsigned int height)
 }
 
 /**
- * strtow - splits a string into words
+ * strtow - splits a string into words.
+ * @str: string.
  *
- * @str: string
- *
- * Return: pointer to an array of strings, otherwise NULL
+ * Return: pointer of an array of integers
  */
 char **strtow(char *str)
 {
@@ -47,20 +44,20 @@ char **strtow(char *str)
 	for (i = a1 = 0; i < height; i++)
 	{
 		for (c = a1; str[c] != '\0'; c++)
+		{
+			if (str[c] == ' ')
+				a1++;
+			if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
 			{
-				if (str[c] == ' ')
-					a1++;
-				if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
+				aout[i] = malloc((c - a1 + 2) * sizeof(char));
+				if (aout[i] == NULL)
 				{
-					aout[i] = malloc((c - a1 + 2) * sizeof(char));
-					if (aout[i] == NULL)
-					{
-						free_gr(aout, i);
-						return (NULL);
-					}
-					break;
+					ch_free_grid(aout, i);
+					return (NULL);
 				}
+				break;
 			}
+		}
 		for (j = 0; a1 <= c; a1++, j++)
 			aout[i][j] = str[a1];
 		aout[i][j] = '\0';
